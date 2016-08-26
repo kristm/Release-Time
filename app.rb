dba7abb8001a743f73591e8e7f4cff3c9e840aef
Krist Menina <krist@quipper.com>
2016-08-26 17:43:32 +0800

    add httparty

--- app.rb
+++ app.rb
@@ -1,17 +1,38 @@
 require 'rubygems'
 require 'sinatra'
+require 'httparty'
 require 'dotenv'
 
-get "/" do
-  "release timer"
-end
+class App < Sinatra::Base
+  set :cache, {}
 
-post "/" do
-  puts self.inspect
-end
+  configure do
+    set :slack_incoming_url, "https://hooks.slack.com/services/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"
+  end
 
-post "/start" do
-  puts "M"*200
-  puts params.inspect
-  puts "M"*200
+  get "/" do
+    "#{App.cache.inspect}"
+  end
+
+  post "/start" do
+    puts "M"*200
+    puts params.inspect
+    puts "M"*200
+    App.cache['start'] = Time.now
+  end
+
+  post "/stop" do
+    App.cache['stop'] = Time.now
+    #curl -X POST --data-urlencode 'payload={"channel": "#release-test", "username": "webhookbot", "text": "This is posted to #release-test and cos from a bot named webhookbot.", "icon_emoji": ":krist:"}' https://hooks.slack.com/services/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
+    HTTParty.post(settings.slack_incoming_url,
+                  body: { 
+                    payload: {
+                      channel: "#release-test",
+                      username: "webhookbot",
+                      text: "Release time: #{App.cache['stop'] - App.cache['start']}",
+                      icon_emoji: ":krist:",
+                    }
+                  }
+                 )
+  end
 end
